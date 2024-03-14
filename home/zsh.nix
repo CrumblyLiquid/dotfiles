c2f1a574562efc4f3cb48d78d260b041672e47e3
CrumblyLiquid <crumblyliquid@gmail.com>
2024-03-14 22:52:41 +0100

    zsh: Add aliases and functions to zsh.nix

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,12 +1,6 @@
 { globals, ... }: {
   programs.zsh = {
     enable = true;
-    autosuggestions.enable = true;
-    histSize = 10000;
-
-    shellAliases = {
-      nv = "nvim";
-    };
 
     ohMyZsh = {
       enable = true;
@@ -19,7 +13,45 @@
       enable = true;
 
       enableCompletion = true;
+      enableAutosuggestions = true;
       syntaxHighlighting.enable = true;
+      autocd = true;
+
+      history.size = 10000;
+
+      shellAliases = {
+        nv = "nvim";
+        dotfiles = "cd ~/.dots";
+        dots = "dotfiles";
+        sl = "sl -e";
+        gpp = "g++";
+        gppwp = "g++ -Wall -pedantic";
+      };
+
+      initExtra = ''
+        # Go up certain amount of directories
+        up () {
+          if [ -z $1 ]; then
+            cd ..
+          elif [ $1 -gt 0 ]; then
+            let count=1
+            cdstr=".."
+            while [ $count -lt $1 ]; do
+              cdstr="$cdstr/.."
+              let count=count+1
+            done
+            cd $cdstr
+          else
+            echo "Argument must be a positive integer."
+          fi
+        }
+
+        run () {
+          g++ -std=c++17 -Wall -pedantic -Wno-long-long -O2 -o "$1.bin" "$1.c";
+          chmod u+x "./$1.bin";
+          ./"$1.bin";
+        }
+      ''
     };
   };
 }
