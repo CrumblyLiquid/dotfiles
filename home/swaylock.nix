b02de7b747512e6acf14509d428d9399e1604eb7
CrumblyLiquid <crumblyliquid@gmail.com>
2024-03-24 14:09:55 +0100

    swaylock:n Fix swaylock not unlocking

--- home/swaylock.nix
+++ home/swaylock.nix
@@ -4,6 +4,9 @@
     swaylock-effects
   ];
 
+  # https://nixos.wiki/wiki/Sway#Swaylock_cannot_be_unlocked_with_the_correct_password
+  security.pam.services.swaylock = {};
+
   home-manager.users."${globals.user}" = {
     home.file = {
       ".config/sway-lock/config" = {
