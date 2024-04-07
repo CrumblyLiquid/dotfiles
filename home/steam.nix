0bab2e40b21ecb0d7dc903a4330aa4f129c90ebb
CrumblyLiquid <crumblyliquid@gmail.com>
2024-04-07 23:03:42 +0200

    steam:n Fix gamescope

--- home/steam.nix
+++ home/steam.nix
@@ -11,6 +11,15 @@
   programs.steam = {
     enable = true;
     gamescopeSession.enable = false;
+    # Gamescope fix
+    # https://www.reddit.com/r/NixOS/comments/1bmj4mz/gamescope_and_steam/
+    # also maybe have to run `sudo chown <username> /tmp/.X11-unix`
+    package = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        libkrb5
+        keyutils
+      ];
+    };
   };
 
   xdg.portal = {
