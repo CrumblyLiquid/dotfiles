c0afb4d27b49f9a0ff1f33848c308f4eb23b04c3
CrumblyLiquid <crumblyliquid@gmail.com>
2024-04-14 23:22:39 +0200

    nvim: Easier mode switching + disable window moving binds

--- nvim/init.lua
+++ nvim/init.lua
@@ -107,6 +107,9 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Write" })
 vim.keymap.set("n", "<leader>q", "<cmd>confirm q<CR>", { desc = "Quit" })
 
+vim.keymap.set("i", "<leader>o", "<Esc>")
+vim.keymap.set("v", "<leader>o", "<Esc>")
+
 -- Open and close the Netrw file explorer
 vim.keymap.set("n", "<leader>fe", vim.cmd.Explore, { desc = "Open explorer" })
 vim.keymap.set("n", "<leader>fw", vim.cmd.Rexplore, { desc = "Close explorer" })
@@ -155,10 +158,10 @@ vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+-- vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+-- vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+-- vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+-- vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
