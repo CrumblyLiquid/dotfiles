75ed9719a847f2d554e08d448db08c29cbcab0ab
CrumblyLiquid <crumblyliquid@gmail.com>
2024-05-17 23:39:09 +0200

    nix: Add deafult apps

--- home/default.nix
+++ home/default.nix
@@ -35,7 +35,34 @@
   };
 
   home-manager.users."${globals.user}" = {
+    xdg.enable = true;
     xdg.configFile."wallpaper.png" = { source = globals.wallpaper; };
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "image/png" = "gwenview.desktop";
+        "image/svg+xml" = "gwenview.desktop";
+        "image/jpeg" = "gwenview.desktop";
+        "image/webp" = "gwenview.desktop";
+        "image/gif" = "gwenview.desktop";
+        "image/avif" = "gwenview.desktop";
+        "image/tiff" = "gwenview.desktop";
+        "image/bmp" = "gwenview.desktop";
+        "image/x-icon" = "gwenview.desktop";
+        "audio/mp4" = "vlc.desktop";
+        "video/mp4" = "vlc.desktop";
+        "audio/mp3" = "vlc.desktop";
+        "inode/directory" = "dolphin.desktop";
+        "application/pdf" = "okular.desktop";
+        "application/zip" = "ark.desktop";
+        "application/tar" = "ark.desktop";
+        "application/gz" = "ark.desktop";
+        "text/plain" = "kate.desktop";
+        "text/csv" = "kate.desktop";
+        "text/html" = "kate.desktop";
+        "text/xml" = "kate.desktop";
+      };
+    };
 
     home.packages = with pkgs; [ ];
     home.file = { };
