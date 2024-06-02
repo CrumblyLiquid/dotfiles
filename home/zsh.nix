48e352a566b662008af0f66c803494ae96749801
CrumblyLiquid <crumblyliquid@gmail.com>
2024-06-02 13:02:08 +0200

    zsh:n Use normal config files

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,57 +1,16 @@
 { globals, ... }: {
   programs.zsh = {
     enable = true;
-
-    ohMyZsh = {
-      enable = true;
-      plugins = [ "git" "sudo" "man" "tmux" ];
-    };
   };
 
   home-manager.users."${globals.user}" = {
-    programs.zsh = {
-      enable = true;
-
-      enableCompletion = true;
-      enableAutosuggestions = true;
-      syntaxHighlighting.enable = true;
-      autocd = true;
-
-      history.size = 10000;
-
-      shellAliases = {
-        nv = "nvim";
-        dotfiles = "cd ~/.dots";
-        dots = "dotfiles";
-        sl = "sl -e";
-        gpp = "g++";
-        gppwp = "g++ -Wall -pedantic";
+    home.file = {
+      "~/.zshrc" = {
+        source = ./../zsh/zshrc;
       };
-
-      initExtra = ''
-        # Go up certain amount of directories
-        up () {
-          if [ -z $1 ]; then
-            cd ..
-          elif [ $1 -gt 0 ]; then
-            let count=1
-            cdstr=".."
-            while [ $count -lt $1 ]; do
-              cdstr="$cdstr/.."
-              let count=count+1
-            done
-            cd $cdstr
-          else
-            echo "Argument must be a positive integer."
-          fi
-        }
-
-        run () {
-          g++ -std=c++17 -Wall -pedantic -Wno-long-long -O2 -o "$1.bin" "$1.c";
-          chmod u+x "./$1.bin";
-          ./"$1.bin";
-        }
-      '';
+      "~/.zshenv" = {
+        source = ./../zsh/zshenv;
+      };
     };
   };
 }
