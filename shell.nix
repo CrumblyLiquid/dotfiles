c03efdb09c7713c2379bd6861e9319946334b398
CrumblyLiquid <crumblyliquid@gmail.com>
2024-06-22 12:55:44 +0200

    nix: Shell

--- shell.nix
+++ shell.nix
@@ -8,10 +8,29 @@ let
     xorg.libXcursor
     xorg.libXi
     xorg.libXrandr
+    cmake
+    alsa-lib
+    binutils
+    pkg-config
+    wrapGAppsHook3
+    glib
+    glibc
+    gsettings-desktop-schemas
+    gtk3
+    gtksourceview4
+    librsvg
+    libsndfile
+    libxml2
+    libzip
+    pcre
+    poppler
+    portaudio
+    zlib
+    lua
   ];
 in
 with pkgs; mkShell {
   inputsFrom = [ ];
-  buildInputs = [ rustup pkg-config python3  ];
+  buildInputs = [  gettext wrapGAppsHook3 glib portaudio cmake binutils alsa-lib cmake alsa-lib binutils glib gsettings-desktop-schemas gtk3 gtksourceview4 librsvg libsndfile libxml2 libzip pcre poppler portaudio zlib lua gtk3 cmake binutils rustup pkg-config python3  ];
   LD_LIBRARY_PATH = "${libPath}";
 }
