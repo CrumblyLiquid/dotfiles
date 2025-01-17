577c1728e3735f84077593b995a0a1ad22cc6b33
CrumblyLiquid <crumblyliquid@gmail.com>
2025-01-17 16:00:58 +0100

    dolphin:n Add missing packages to make dolphin work better

--- nix/home/apps.nix
+++ nix/home/apps.nix
@@ -11,6 +11,17 @@
 
     kdePackages.dolphin
     kdePackages.dolphin-plugins
+    # From https://wiki.nixos.org/wiki/Dolphin
+    # For wayland support
+    kdePackages.qtwayland
+    # For correct svg icons rendering
+    kdePackages.qtsvg
+    # Mounting network shares
+    kdePackages.kio-fuse
+    kdePackages.kio-extras
+    # Fix file associations
+    kdePackages.plasma-workspace
+
     kdePackages.ark
     kdePackages.karchive
     kdePackages.kate
@@ -38,6 +49,9 @@
     prismlauncher
     lunar-client
   ];
+
+  # Fix file associations for Dolphin and other KDE apps
+  environment.etc."/xdg/menus/plasma-applications.menu".text = builtins.readFile "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
 
   services.fwupd.enable = true;
 }
