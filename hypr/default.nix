be208e030299ddbc036771bfc2c043248d76a757
CrumblyLiquid <crumblyliquid@gmail.com>
2025-02-01 17:15:13 +0100

    hypr: Multi-monitor setup

--- hypr/default.nix
+++ hypr/default.nix
@@ -106,14 +106,13 @@
       };
     };
 
-    xdg.portal = {
-      extraPortals = [ pkgs.inputs.hyprland.xdg-desktop-portal-hyprland ];
-      configPackages = [ pkgs.inputs.hyprland.hyprland ];
-    };
-
     wayland.windowManager.hyprland = {
       enable = true;
       xwayland.enable = true;
+
+      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+      portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+
       settings = {
         source = [
           # "~/.config/hypr/autostart.conf"
