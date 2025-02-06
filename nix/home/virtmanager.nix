991fd2f4cc08d928339db43b1e2e24fae740400d
CrumblyLiquid <crumblyliquid@gmail.com>
2025-02-06 15:53:53 +0100

    nix: Virtual machines fixes

--- nix/home/virtmanager.nix
+++ nix/home/virtmanager.nix
@@ -14,17 +14,29 @@
         swtpm.enable = true;
       };
     };
+    spiceUSBRedirection.enable = true;
   };
   users.groups.libvirtd = {
     members = [ "${globals.user}" ];
   };
-  environment.sessionVariables.LIBVIRT_DEFAULT_URI = [ "qemu:///system" ];
+  # environment.sessionVariables.LIBVIRT_DEFAULT_URI = [ "qemu:///system" ];
+  home-manager.users."${globals.user}" = {
+    dconf.settings = {
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = ["qemu:///system"];
+        uris = ["qemu:///system"];
+      };
+    };
+  };
   programs.virt-manager.enable = true;
 
+  # https://github.com/NixOS/nixpkgs/issues/363887#issuecomment-2536693220
+  boot.kernelParams = [ "kvm.enable_virt_at_load=0" ];
+
   virtualisation.virtualbox.host.enable = true;
-  # virtualisation.virtualbox.host.enableExtensionPack = true;
-  # virtualisation.virtualbox.guest.enable = true;
-  # virtualisation.virtualbox.guest.dragAndDrop = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.guest.enable = true;
+  virtualisation.virtualbox.guest.dragAndDrop = true;
   virtualisation.virtualbox.guest.clipboard = true;
   users.extraGroups.vboxusers = {
     members = [ "${globals.user}" ];
