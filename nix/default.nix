6b70e30a9f135bb3fc1d24cc92dc5021105a4947
CrumblyLiquid <crumblyliquid@gmail.com>
2025-04-21 17:43:52 +0200

    nix: Switch to Linux LTS

--- nix/default.nix
+++ nix/default.nix
@@ -2,7 +2,8 @@
   imports = [ ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # Use LTS instead
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
