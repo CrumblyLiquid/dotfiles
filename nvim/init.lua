6ca142ab5a2e9dcc623a5cf968b7f0117b3febce
CrumblyLiquid <crumblyliquid@gmail.com>
2025-05-25 19:52:24 +0200

    nvim: Flatten configuration structure

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,11 @@
-require("config.lazy")
-require("config.options")
-require("config.keybinds")
+-- require("folder.file") loads the lua file
+-- at lua/folder/file.lua (relative to init.lua)
+
+-- Load `lazy.nvim` - package manager
+require("lazy")
+
+-- Basic options
+require("options")
+
+-- Basic keybinds
+require("keybinds")
