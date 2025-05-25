744fe84a6a1d839f98b826827132ff13fd3fd106
CrumblyLiquid <crumblyliquid@gmail.com>
2025-05-25 19:55:19 +0200

    nvim: Remove keywords from completions and remove some default comments

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -6,7 +6,7 @@ return {
     dependencies = {
       {
         "L3MON4D3/LuaSnip",
-        version = "2.*",
+        version = "v2.*",
         build = (function()
           -- Not supported on Windows and requires the `make` binary
           if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
@@ -30,30 +30,12 @@ return {
     },
     opts = {
       keymap = {
-        -- 'default' (recommended) for mappings similar to built-in completions
-        --   <c-y> to accept ([y]es) the completion.
-        --    This will auto-import if your LSP supports it.
-        --    This will expand snippets if the LSP sent a snippet.
-        -- 'super-tab' for tab to accept
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
-        --
-        -- For an understanding of why the 'default' preset is recommended,
-        -- you will need to read `:help ins-completion`
-        --
-        -- No, but seriously. Please read `:help ins-completion`, it is really good!
-        --
-        -- All presets have the following mappings:
-        -- <tab>/<s-tab>: move to right/left of your snippet expansion
-        -- <c-space>: Open menu or open docs if already open
-        -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-        -- <c-e>: Hide menu
-        -- <c-k>: Toggle signature help
-        --
-        -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
 
+        -- Accept the current item
         ['<C-space>'] = { 'select_and_accept', 'fallback' },
+
+        -- Show documentation
         ['<C-y>'] = { 'show', 'show_documentation', 'hide_documentation' },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
@@ -61,34 +43,43 @@ return {
       },
 
       appearance = {
-        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = 'mono',
       },
 
       completion = {
-        -- By default, you may press `<c-space>` to show the documentation.
-        -- Optionally, set `auto_show = true` to show the documentation after a delay.
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        documentation = {
+          -- Can show documentation after delay
+          auto_show = false,
+          auto_show_delay_ms = 500
+        },
       },
 
       sources = {
         default = { 'lsp', 'path', 'snippets', 'lazydev' },
         providers = {
-          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          lsp = {
+            module = 'blink.cmp.sources.lsp',
+            -- Exclude keywords/constants from autocomplete
+            -- https://cmp.saghen.dev/recipes.html#exclude-keywords-constants-from-autocomplete
+            transform_items = function(_, items)
+              return vim.tbl_filter(function(item)
+                return item.kind ~= require('blink.cmp.types').CompletionItemKind.Keyword
+              end, items)
+            end,
+          },
+          lazydev = {
+            module = 'lazydev.integrations.blink',
+            score_offset = 100
+          },
         },
       },
 
+      -- Use LuaSnip for snippets
+      -- (also loads any snippets found at `snippets/`)
       snippets = { preset = 'luasnip' },
 
-      -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-      -- which automatically downloads a prebuilt binary when enabled.
-      --
-      -- By default, we use the Lua implementation instead, but you may enable
-      -- the rust implementation via `'prefer_rust_with_warning'`
-      --
-      -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = 'lua' },
+      -- Use the better rust fuzzy search
+      fuzzy = { implementation = 'rust' },
 
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
