6edb5a174ceb3c30b0c66e606ca056136fd0d393
CrumblyLiquid <crumblyliquid@gmail.com>
2025-05-26 11:34:52 +0200

    nix: Use newer version of tofi

--- tofi/default.nix
+++ tofi/default.nix
@@ -1,15 +1,23 @@
 { globals, pkgs, config, ... }: {
+  # Updated version of tofi
+  # (supports the matching-algorithm option)
+  nixpkgs.overlays = [
+    (final: prev:
+      {
+        tofi = prev.tofi.overrideAttrs (old: {
+          src = prev.fetchFromGitHub {
+            owner = "philj56";
+            repo = "tofi";
+            rev = "1eb6137572ab6c257ab6ab851d5d742167c18120";
+            hash = "sha256-OD56rwDrXgb5pg85sT5v+zl9A1/sfn77PBSG4gT76bE=";
+          };
+        });
+      }
+    )
+  ];
+
   environment.systemPackages = with pkgs; [
     tofi
   ];
-
-  # home-manager.users."${globals.user}" = {
-  #   home.file = {
-  #     ".config/tofi" = {
-  #       source = ./.;
-  #       recursive = true;
-  #     };
-  #   };
-  # };
 }
 
