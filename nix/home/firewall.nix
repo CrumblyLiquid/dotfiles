df05b6c5d05883882698a4b08346356532200b3c
CrumblyLiquid <crumblyliquid@gmail.com>
2025-05-26 11:34:10 +0200

    nix: Remove Immich firewall rule

--- nix/home/firewall.nix
+++ nix/home/firewall.nix
@@ -1,11 +1,7 @@
 { globals, inputs, pkgs, ... }: {
   networking.firewall = {
     enable = true;
-    # Immich
-    allowedTCPPorts = [ 2283 ];
   };
-
-  # Possibly use https://wiki.nixos.org/wiki/Immich
 }
 
 
