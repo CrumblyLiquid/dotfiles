bf1752c44e5a6c57cff5956d2227c228b93f158c
CrumblyLiquid <crumblyliquid@gmail.com>
2025-05-26 11:34:33 +0200

    nix: Android dev stuff

--- nix/home/android.nix
+++ nix/home/android.nix
@@ -2,9 +2,9 @@
   # environment.enableDebugInfo = true;
   environment.systemPackages = with pkgs; [
     android-tools
-    # android-studio
-    # android-studio-tools
-    # flutter
+    android-studio
+    android-studio-tools
+    flutter
   ];
 
   nixpkgs.config.android_sdk.accept_license = true;
