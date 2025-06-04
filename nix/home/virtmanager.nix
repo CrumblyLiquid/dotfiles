4bd08347e1a046d91d0cfb24a5c8ff39bc88e913
CrumblyLiquid <crumblyliquid@gmail.com>
2025-06-04 17:34:22 +0200

    nix: Fix long startup

--- nix/home/virtmanager.nix
+++ nix/home/virtmanager.nix
@@ -35,9 +35,6 @@
 
   virtualisation.virtualbox.host.enable = true;
   virtualisation.virtualbox.host.enableExtensionPack = true;
-  virtualisation.virtualbox.guest.enable = true;
-  virtualisation.virtualbox.guest.dragAndDrop = true;
-  virtualisation.virtualbox.guest.clipboard = true;
   users.extraGroups.vboxusers = {
     members = [ "${globals.user}" ];
   };
