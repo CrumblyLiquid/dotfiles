c7223d9898d2bd08fc4b1c887a592973710871b9
CrumblyLiquid <crumblyliquid@gmail.com>
2025-07-19 20:46:12 +0200

    nix: Add neovim langauge stuff as nix packages

--- nvim/default.nix
+++ nvim/default.nix
@@ -1,5 +1,35 @@
-{ globals, pkgs, ... }:
-{
+{ globals, pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    # Neovim stuff
+    neovim
+
+    # Language stuff
+    # TODO: Language options flake (with preinstalled lsps, formatters, etc)
+    ast-grep
+
+    # Lua
+    luajitPackages.lua-lsp
+    # Rust
+    rust-analyzer
+    # TODO: Bacon, Bacon-ls (https://github.com/crisidev/bacon-ls?tab=readme-ov-file#nix)
+    # C/C++
+    clang-tools
+    # Python
+    pyright # TODO: basedpyright
+    # Bash
+    bash-language-server
+    # JSON
+    nodePackages.vscode-json-languageserver
+    # Yaml
+    yaml-language-server
+    # Typst
+    tinymist # LSP
+    typstyle # Formatter
+    # Nix
+    nil
+  ];
+
+
   home-manager.users."${globals.user}" = {
     programs.neovim = {
       enable = true;
