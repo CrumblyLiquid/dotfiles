173221f3a1fed719a832d2b1174ecaa6077c5442
CrumblyLiquid <crumblyliquid@gmail.com>
2025-07-27 14:26:28 +0200

    nvim: Add render-markdown plugin for better markdown editing

--- nvim/lua/plugins/preview.lua
+++ nvim/lua/plugins/preview.lua
@@ -22,4 +22,11 @@ return {
       { "<leader>pc", function() require('omni-preview').command({ args = "stop" }) end,  desc = "Stop preview" },
     }
   },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  }
 }
