5633d0112364f63cb9c9222cd387533052391c14
CrumblyLiquid <crumblyliquid@gmail.com>
2025-11-26 19:46:11 +0100

    nvim: Better syntax highlighting for lambdalus

--- nvim/queries/lambdalus/highlights.scm
+++ nvim/queries/lambdalus/highlights.scm
@@ -1,5 +1,9 @@
-(variable) @variable
+; (variable) @variable
+
 (macro_name) @function.macro
 (lambda) @keyword.function
-(lambda_expression
-  parameter: (variable) @variable.parameter)
+
+(open_paren) @punctuation.bracket
+(close_paren) @punctuation.bracket
+
+(parameter) @variable.parameter
