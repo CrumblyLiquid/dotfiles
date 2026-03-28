2cccf1f1eb32296c80ce9cccedf499fbc2925941
CrumblyLiquid <crumblyliquid@gmail.com>
2026-03-28 19:56:14 +0100

    sync: Update devices

--- nix/home/syncthing.nix
+++ nix/home/syncthing.nix
@@ -18,35 +18,48 @@
       };
       # https://docs.syncthing.net/users/faq.html#should-i-keep-my-device-ids-secret
       devices = {
-        "laptop" = {
+        nas = {
+          name = "NAS";
+          id = "OQKQMLN-YVROQRO-RHQFRVI-7BDSYEW-M7WOH3U-NC2QRQ5-HPLIBR6-V5JBDA5";
+        };
+        laptop = {
+          name = "Laptop";
           id = "FDM5BL2-TLAVPQI-23FVPUJ-TNFJYPX-JBGI3SU-U2XGMNO-QFPVP6O-HKRTEA4";
         };
-        "mobile" = {
+        mobile = {
+          name = "Mobile";
           id = "3A35K6U-NKQJIDT-F2X42OZ-5WJP4KS-V53TXBG-VCAEP6C-CQF4X4J-SHFYFAO";
+        };
+        backup = {
+          name = "Backup Mobile";
+          id = "YYP2KOM-OT26MVS-X5USZA6-H3SKRJ2-WP2C2Q5-F75MJKK-CFUHBB6-WJBFFQ2";
         };
       };
       folders = {
         "Misc" = {
           path = "/home/${globals.user}/Misc";
           devices = [
+            "nas"
             "laptop"
             "mobile"
+            "backup"
           ];
         };
         "Books" = {
           path = "/home/${globals.user}/Books";
           devices = [
+            "nas"
             "laptop"
             "mobile"
+            "backup"
           ];
         };
         "School" = {
           path = "/home/${globals.user}/School";
-          devices = [ "laptop" ];
-        };
-        "Photos" = {
-          path = "/home/${globals.user}/Pictures/Pixel";
-          devices = [ "mobile" ];
+          devices = [
+            "nas"
+            "laptop"
+          ];
         };
       };
     };
